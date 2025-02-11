c7395d50939b08e76f25b1130e58a26c9d5cc1ae
rootkiti <robertu20002000@gmail.com>
2025-02-12 00:18:09 +0200

    creating cards

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import wrangle as wr
 import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
-
+from streamlit_card import card
 
 # reading data
 df = pd.read_excel("VIBE.xlsx", sheet_name=None)
@@ -16,18 +16,19 @@ df = wr.wrangle(data)
 stl.set_page_config(page_title="Tool",
                     page_icon=":bar_chart:", layout="wide")
 stl.title("Lests start")
-
 stl.markdown(
     '<style>div.block-container{padding-top:1rem;}</style>', unsafe_allow_html=True)
 
 # sidebar
 stl.sidebar.header("Select Your Filter")
 project = stl.sidebar.selectbox(
-    
-    
+
+
     "Select Project", df["Projects"].unique(), index=None)
 
-# deviding page into two columns 
+# deviding page into two columns
+kpi1, kpi2, kpi3, kpi4 = stl.columns(4)
+
 outreach_col, supported_col = stl.columns(2)
 if project != None:
     filtered_df = df.loc[df['Projects'] == project]
@@ -36,7 +37,7 @@ if project != None:
     refuge = wr.refuge(filtered_df)
     community = wr.community(filtered_df)
     pwd = wr.pwd(filtered_df)
-    
+
     col_1, col_2 = stl.columns(2)
     with col_1:
         # ============================================ cumulative ==============================
@@ -70,7 +71,7 @@ if project != None:
             width=400
         )
         stl.plotly_chart(fig)
-        
+
         stl.write()
 # ====================================== Refuges =====================================================
         categories = refuge.columns
@@ -102,7 +103,7 @@ if project != None:
             width=400
         )
         stl.plotly_chart(fig)
-        
+
         stl.write()
 # ====================================== pwd ===================================================
         categories = pwd.columns
@@ -166,7 +167,7 @@ if project != None:
             width=400
         )
         stl.plotly_chart(fig)
-        
+
         stl.write()
         # ========================================= comminity ===============================
         categories = community.columns
@@ -200,28 +201,91 @@ if project != None:
         stl.plotly_chart(fig)
 
 else:
-    with outreach_col:
-        fig = px.bar(df[['Projects', 'Outreach']].sort_values(
-            by='Outreach'), x='Projects', y='Outreach', text='Outreach', title='Outreach per Project')
+    with stl.container():
+        with kpi1:
 
-        # Customize layout
-        fig.update_traces(textposition='outside', marker_color='royalblue')
-        fig.update_layout(
-            xaxis_tickangle=-45,
-            width=400,
-            height=540
-        )
-        stl.plotly_chart(fig)
+            card1 = card(
+                title="1000",
+                text="This is a test card",
+                styles={
+                    "card": {
+                        "width": "120%",
+                        "height": "100px",
+                        "background-color": "#f0f8ff",  # Add your desired background color
+                        "border-radius": "5px",
 
-    with supported_col:
-        fig = px.bar(df[['Projects', 'Supported(MSMEs)']].sort_values(
-            by='Supported(MSMEs)'), x='Projects', y='Supported(MSMEs)', text='Supported(MSMEs)', title='Supported(MSMEs) per Project')
+                    }
+                }
+            )
+        with kpi2:
 
-        # Customize layout
-        fig.update_traces(textposition='outside', marker_color='royalblue')
-        fig.update_layout(
-            xaxis_tickangle=-45,
-            width=400,
-            height=540
-        )
-        stl.plotly_chart(fig)
+            card2 = card(
+                title="2000",
+                text="This is a tefst card",
+                styles={
+                    "card": {
+                        "width": "120%",
+                        "height": "100px",
+                        "background-color": "#f0f8ff",  # Add your desired background color
+                        "border-radius": "5px",
+
+                    }
+                }
+            )
+        with kpi3:
+
+            card3 = card(
+                title="3000",
+                text="This is a tesst card",
+                styles={
+                    "card": {
+                        "width": "120%",
+                        "height": "100px",
+                        "background-color": "#f0f8ff",  # Add your desired background color
+                        "border-radius": "5px",
+
+                    }
+                }
+            )
+        with kpi4:
+
+            card4 = card(
+                title="40000",
+                text="This is a txest card",
+                styles={
+                    "card": {
+                        "width": "120%",
+                        "height": "100px",
+                        "background-color": "#f0f8ff",  # Add your desired background color
+                        "border-radius": "5px",
+
+                    }
+                }
+            )
+    with stl.container():
+        with outreach_col:
+            
+            fig = px.bar(df[['Projects', 'Outreach']].sort_values(
+                by='Outreach'), x='Projects', y='Outreach', text='Outreach', title='Outreach per Project')
+
+            # Customize layout
+            fig.update_traces(textposition='outside', marker_color='royalblue')
+            fig.update_layout(
+                xaxis_tickangle=-45,
+                width=400,
+                height=540
+            )
+            stl.plotly_chart(fig)
+
+        with supported_col:
+            fig = px.bar(df[['Projects', 'Supported(MSMEs)']].sort_values(
+                by='Supported(MSMEs)'), x='Projects', y='Supported(MSMEs)', text='Supported(MSMEs)', title='Supported(MSMEs) per Project')
+
+            # Customize layout
+            fig.update_traces(textposition='outside', marker_color='royalblue')
+            fig.update_layout(
+                xaxis_tickangle=-45,
+                width=400,
+                height=540
+            )
+            stl.plotly_chart(fig)
